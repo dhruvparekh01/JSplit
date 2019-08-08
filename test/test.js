6925bd780f3be3da6f078d15fac6a7911d6175ad
Josh W <jtwong10@gmail.com>
2019-08-07 20:31:46 -0700

    user sign in test: needs fixing

--- test/test.js
+++ test/test.js
@@ -1,13 +1,63 @@
 const test = require("firebase-functions-test")(
   {
-    databaseURL: "https://test-6ec85.firebaseio.com",
-    projectId: "test-6ec85",
-    storageBucket: "test-6ec85.appspot.com"
+    databaseURL: "https://jsplit-34d67.firebaseio.com",
+    projectId: "jsplit-34d67",
+    storageBucket: "jsplit-34d67.appspot.com"
   },
-  "../serviceAccountKey.json"
+  "serviceAccountKey.json"
 );
+const firebase = require("firebase");
+const a = require("firebase/storage");
+var config = {
+  apiKey: "AIzaSyADqxirXJzsVOo2b-U_Bb1wfAcDgRMmcNw",
+  authDomain: "jsplit-34d67.firebaseapp.com",
+  databaseURL: "https://jsplit-34d67.firebaseio.com",
+  projectId: "jsplit-34d67",
+  storageBucket: "",
+  messagingSenderId: "515865367088",
+  appId: "1:515865367088:web:e455c8fdb0435044"
+};
+
+var fb = firebase.initializeApp(config);
+var db = firebase.firestore();
 var assert = require("assert");
 
+// Create a DataSnapshot with the value 'input' and the reference path 'messages/11111/original'.
+const snap = test.database.makeDataSnapshot("input", "messages/11111/original");
+console.log(snap);
+
+describe("User Signup Tests", function() {
+  /* Function to add a new user */
+  var email = "55testynewuser@testing.ohyes";
+  var password = "testing123";
+  var name = "Tester Jim";
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(function() {
+      db.collection("users")
+        .doc(firebase.auth().currentUser.uid)
+        .set({
+          Name: name,
+          Email: email,
+          UID: firebase.auth().currentUser.uid,
+          Groups: []
+        });
+    });
+  firebase.auth().signInWithEmailAndPassword(email, password);
+
+  describe("Check for newly made account", function() {
+    it("should return user ID if account has been created", function() {
+      assert.equal(firebase.auth().currentUser.Email, email);
+    });
+
+    // test.cleanup() should remove environment variables and deletes firebase apps used for testing
+    test.cleanup();
+  });
+});
+
+// testing out some tests
 describe("Array", function() {
   describe("#indexOf()", function() {
     it("should return -1 when the value is not present", function() {
