796d88b2dee2ed28bcb3fe1fcaea95aa0930fc09
Josh W <jtwong10@gmail.com>
2019-08-07 17:49:55 -0700

    testing some tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,24 @@
 var assert = require("assert");
+
 describe("Array", function() {
   describe("#indexOf()", function() {
     it("should return -1 when the value is not present", function() {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
+  });
+});
+
+describe("Basic Mocha String Test", function() {
+  it("should return number of charachters in a string", function() {
+    assert.equal("Hello".length, 5);
+  });
+  it("should return first charachter of the string", function() {
+    assert.equal("Hello".charAt(0), "H");
+  });
+});
+
+describe("Jimbob is the best Bob", function() {
+  it("should return yes", function() {
+    assert.equal("yes", "yes");
   });
 });
