a66f2b7b868f8fbc4da9bc76815c75032489124d
Josh W <jtwong10@gmail.com>
2019-08-13 14:42:47 -0700

    update

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const firebase = require('firebase');
-const bodyParser = require('body-parser');
-const hbs = require('hbs');
+const express = require("express");
+const firebase = require("firebase");
+const bodyParser = require("body-parser");
+const hbs = require("hbs");
 
 const port = process.env.PORT || 8000;
-const path = require('path');
+const path = require("path");
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
 /*
 app.set('view engine', 'hbs');
@@ -21,24 +21,26 @@ hbs.registerPartials(path.join(__dirname, '/views/partials'));
 //Body Parser
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 
 /**
  * Firebase Stuff
  */
 var cur_user;
 
-const a = require('firebase/storage');
+const a = require("firebase/storage");
 var config = {
-    apiKey: "AIzaSyADqxirXJzsVOo2b-U_Bb1wfAcDgRMmcNw",
-    authDomain: "jsplit-34d67.firebaseapp.com",
-    databaseURL: "https://jsplit-34d67.firebaseio.com",
-    projectId: "jsplit-34d67",
-    storageBucket: "",
-    messagingSenderId: "515865367088",
-    appId: "1:515865367088:web:e455c8fdb0435044"
+  apiKey: "AIzaSyADqxirXJzsVOo2b-U_Bb1wfAcDgRMmcNw",
+  authDomain: "jsplit-34d67.firebaseapp.com",
+  databaseURL: "https://jsplit-34d67.firebaseio.com",
+  projectId: "jsplit-34d67",
+  storageBucket: "",
+  messagingSenderId: "515865367088",
+  appId: "1:515865367088:web:e455c8fdb0435044"
 };
 
 var fb = firebase.initializeApp(config);
@@ -47,140 +49,154 @@ var db = firebase.firestore();
 /**
  * Endpoints
  */
-app.get('/', (request, response) => {
-    response.render('index.hbs', {
-        title: 'Landing Page'
-    });
+app.get("/", (request, response) => {
+  response.render("index.hbs", {
+    title: "Landing Page"
+  });
 });
 
-app.get('/signup', (request, response) => {
-    response.render('signup.hbs', {
-        title: 'Sign Up'
-    });
+app.get("/signup", (request, response) => {
+  response.render("signup.hbs", {
+    title: "Sign Up"
+  });
 });
 
-app.get('/signin', (request, response) => {
-    response.render('signin.hbs', {
-        title: 'Sign In'
-    });
+app.get("/signin", (request, response) => {
+  response.render("signin.hbs", {
+    title: "Sign In"
+  });
 });
 
-app.post('/newUser', (request, response) => {
-    /* Function to add a new user */
-    var email = request.body.email;
-    var password1 = request.body.password1;
-    var password2 = request.body.password2;
-    var name = request.body.name;
-    cur_user = email;
+app.post("/newUser", (request, response) => {
+  /* Function to add a new user */
+  var email = request.body.email;
+  var password1 = request.body.password1;
+  var password2 = request.body.password2;
+  var name = request.body.name;
+  cur_user = email;
 
-
-    if (password1 === password2) {
-        firebase.auth().createUserWithEmailAndPassword(email, password1) // firebase Authentication
-            .then(function () {
-                /* If firebase Authentication is successful, make a table for the user.
+  if (password1 === password2) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password1) // firebase Authentication
+      .then(function() {
+        /* If firebase Authentication is successful, make a table for the user.
                 The name of the table is the user's email. This table represents the
                 user's contact list. Add a demo user to this table.*/
-                db.collection(email).add({
-                        Name: 'Demo User',
-                        Email: 'Demo email',
-                        Balance: 0
-                    })
-                    .then(function (docRef) {
-                        console.log("Document written with ID: ", docRef.id);
-                    })
-                    .catch(function (error) {
-                        console.error("Error adding document: ", error);
-                    });
+        db.collection(email)
+          .add({
+            Name: "Demo User",
+            Email: "Demo email",
+            Balance: 0
+          })
+          .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+          })
+          .catch(function(error) {
+            console.error("Error adding document: ", error);
+          });
 
+        /* Add the user to the "all_users" table*/
+        db.collection("all_users")
+          .add({
+            Name: name,
+            Email: email
+          })
+          .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+          })
+          .catch(function(error) {
+            console.error("Error adding document: ", error);
+          });
 
-                /* Add the user to the "all_users" table*/
-                db.collection('all_users').add({
-                        Name: name,
-                        Email: email,
-                    })
-                    .then(function (docRef) {
-                        console.log("Document written with ID: ", docRef.id);
-                    })
-                    .catch(function (error) {
-                        console.error("Error adding document: ", error);
-                    });
-
-
-                //Now render the custom page created for the user
-                response.render('user.hbs', {});
-            })
-            .catch(function (error) {
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                console.log(errorMessage);
-            });
-    }
+        //Now render the custom page created for the user
+        response.render("user.hbs", {});
+      })
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  }
 });
-
 
 // Function for Signing in the returning users
-app.post('/retUser', (request, response) => {
-    var email = request.body.email;
-    var password1 = request.body.password1;
-    cur_user = email; // The current user variable maintains the email of the
-    // user signing for further functions
+app.post("/retUser", (request, response) => {
+  var email = request.body.email;
+  var password1 = request.body.password1;
+  cur_user = email; // The current user variable maintains the email of the
+  // user signing for further functions
 
-    firebase.auth().signInWithEmailAndPassword(email, password1) // firebase sign in
-        .then(function () {
-            email = firebase.auth().currentUser.email; // get logged in user's email
-            response.render('user.hbs', {
-                user: email
-            });
-        })
-        .catch(function (error) {
-            response.render('login.hbs', {
-                message: error.message
-            });
-            console.log("Error with code:", error.code, "\nWith message:", error.message);
-        });
-
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password1) // firebase sign in
+    .then(function() {
+      email = firebase.auth().currentUser.email; // get logged in user's email
+      response.render("user.hbs", {
+        user: email
+      });
+    })
+    .catch(function(error) {
+      response.render("login.hbs", {
+        message: error.message
+      });
+      console.log(
+        "Error with code:",
+        error.code,
+        "\nWith message:",
+        error.message
+      );
+    });
 });
 
-
-
 // Function to add new people in the contact list
-app.post('/search', (request, response) => {
-    var flag = 0
-    var result = ''
+app.post("/search", (request, response) => {
+  var flag = 0;
+  var result = "";
 
-    // console.log('Got email: '+email);
-    var docRef = firebase.firestore().collection("all_users").doc()
-    firebase.firestore().collection("all_users").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            var email = request.body.user_name // Get the email of the user to addeds
+  // console.log('Got email: '+email);
+  var docRef = firebase
+    .firestore()
+    .collection("all_users")
+    .doc();
+  firebase
+    .firestore()
+    .collection("all_users")
+    .get()
+    .then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        var email = request.body.user_name; // Get the email of the user to addeds
 
-            if (email == doc.data().Email) // If a matching email is found in the database
-            {
-                db.collection(cur_user).add({
-                    // adding the found user to contacts
-                    Name: doc.data().Name,
-                    Email: doc.data().Email,
-                    Balance: 0
-                }).then(function (docRef) {
-                    result = doc.data().Name + 'has been added to your contacts.';
-                    flag = 1;
-                }).catch(function (error) {
-                    console.error("Error adding document: ", error);
-                });
-            }
-        });
+        if (email == doc.data().Email) {
+          // If a matching email is found in the database
+          db.collection(cur_user)
+            .add({
+              // adding the found user to contacts
+              Name: doc.data().Name,
+              Email: doc.data().Email,
+              Balance: 0
+            })
+            .then(function(docRef) {
+              result = doc.data().Name + "has been added to your contacts.";
+              flag = 1;
+            })
+            .catch(function(error) {
+              console.error("Error adding document: ", error);
+            });
+        }
+      });
     });
 
-    email = firebase.auth().currentUser.email; // get logged in user's email
+  email = firebase.auth().currentUser.email; // get logged in user's email
 
-    response.render('user.hbs', {
-        user: email,
-        result: result
-    });
+  response.render("user.hbs", {
+    user: email,
+    result: result
+  });
 });
 
 /* Start Server */
 
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
